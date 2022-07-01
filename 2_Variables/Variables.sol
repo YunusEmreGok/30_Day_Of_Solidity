c3a461e05ea9d9ed8685f92e97c441c52d2f1f22
yunusemregok <63564212+YunusEmreGok@users.noreply.github.com>
2022-07-01 15:49:47 +0300

    test

---
+++ 2_Variables/Variables.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.7;
+contract Variables{
+
+    // bool : true and false
+    bool status = true;
+
+    // int : integer number , not including negative numbers
+
+    int256 inttype = -1;
+    
+    //uint : unsigned integer number , including the int difference in negative numbers.
+    uint256 uinttype = 1;
+
+
+}
